46bfa77cb0eed157bee608a286e76cdc68758ce8
georgijbudik <georgijbudik@gmail.com>
2024-03-05 23:32:00 +0100

    feat: add cookie session and connect to mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,10 +6,21 @@ import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 const app = express();
 
+app.set("trust proxy", true);
+
 app.use(express.json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signupRouter);
@@ -21,6 +32,20 @@ app.all("*", async (req, res) => {
 });
 
 app.use(errorHandler);
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDb");
+  } catch (error) {
+    console.log(error);
+  }
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+};
+
+start();
