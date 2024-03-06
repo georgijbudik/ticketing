8b207d4c57525aab4118c41ef2eb587a6d9abe2d
georgijbudik <georgijbudik@gmail.com>
2024-03-06 18:53:11 +0100

    fix: move app init to app file

---
+++ auth/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import "express-async-errors";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
+import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+import cookieSession from "cookie-session";
+
+const app = express();
+
+app.set("trust proxy", true);
+
+app.use(express.json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
