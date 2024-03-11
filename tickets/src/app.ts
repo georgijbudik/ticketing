9af95c9d301c9ff375d07a5c9c128275bd11e09b
georgijbudik <georgijbudik@gmail.com>
2024-03-11 22:47:16 +0100

    feat: add create ticket route and func

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from "express";
 import "express-async-errors";
 
 import cookieSession from "cookie-session";
-import { NotFoundError, errorHandler } from "@gbtickets1/common";
+import { NotFoundError, currentUser, errorHandler } from "@gbtickets1/common";
+import { createTicketRouter } from "../routes/new";
 
 const app = express();
 
@@ -16,6 +17,9 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+
+app.use(currentUser);
+app.use(createTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
